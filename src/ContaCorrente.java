63d4c0c09dc6fbdab18bab5bda4b3ed416cf482d
Rafael Diego <rafael_dso@hotmail.com>
2015-10-14 20:55:03 -0300

    Capitulo 10 - Exercicios Opcionais

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,14 +1,18 @@
-
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements Tributavel {
 
 	public ContaCorrente() {
-		// TODO Auto-generated constructor stub
-	}
-	public ContaCorrente(String titular, int numero, int agencia){
-		super(titular,numero,agencia);
-	}
-	public void atualiza(double taxa){
-		this.setSaldo(this.getSaldo() * (taxa*2));
+
 	}
 
+	public ContaCorrente(String titular, int numero, int agencia) {
+		super(titular, numero, agencia);
+	}
+
+	public void atualiza(double taxa) {
+		this.setSaldo(this.getSaldo() * (taxa * 2));
+	}
+
+	public double calculaTributos() {
+		return this.getSaldo() * 0.01;
+	}
 }
---
+++ src/GerenciadorDeImpostoDeRenda.java
@@ -0,0 +1,14 @@
+class GerenciadorDeImpostoDeRenda {
+	private double total;
+
+	void adiciona(Tributavel t) {
+		System.out.println("Adicionando Tributável: " + t);
+
+		this.total += t.calculaTributos();
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+}
